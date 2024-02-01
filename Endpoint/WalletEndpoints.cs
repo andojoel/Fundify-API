81424fafafbab5f3ee5d79f7b4e9ad2f97c0815d
andojoel <ando2joel@gmail.com>
2024-02-01 16:56:02 +0100

    Update : add GetWallets endpoint

--- Endpoint/WalletEndpoints.cs
+++ Endpoint/WalletEndpoints.cs
@@ -1,5 +1,11 @@
 ﻿using Carter;
+using Fundify_API.DataModels;
 using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Asn1.Ocsp;
+using Org.BouncyCastle.Crypto;
+using System.Security.Claims;
+using System;
+using Fundify_API.Data;
 
 namespace Fundify_API.Endpoint
 {
@@ -9,7 +15,22 @@ namespace Fundify_API.Endpoint
         {
             var group = app.MapGroup("/wallet").RequireAuthorization();
 
-            group.MapGet("", () => Results.Ok("You get your wallet"));
+            group.MapGet("", GetWallets);
+        }
+
+        public static IResult GetWallets(
+            DatabaseContext db,
+            ClaimsPrincipal user)
+        {
+            try
+            {
+                var wallets = db.WalletTable.Where(w => w.ownerUserName == user.Identity!.Name);
+                return Results.Ok(RequestDto.Success("Done", wallets));
+            }
+            catch (Exception e)
+            {
+                return Results.BadRequest(RequestDto.Failed("Error matching data"));
+            }
         }
     }
 }
